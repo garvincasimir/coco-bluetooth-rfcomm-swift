e4d519e1b8cceda1b0bab281f73f8349293b1566
Garvin Casimir <gjcasimir@gmail.com>
2015-02-08 16:47:52 -0500

    Initial Commit

---
+++ Cocoa Bluetooth RFCOMM Swift/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Cocoa Bluetooth RFCOMM Swift
+//
+//  Created by Worker PC on 2/8/15.
+//  Copyright (c) 2015 Garvin Casimir. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Cocoa Bluetooth RFCOMM SwiftTests/Cocoa_Bluetooth_RFCOMM_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Cocoa_Bluetooth_RFCOMM_SwiftTests.swift
+//  Cocoa Bluetooth RFCOMM SwiftTests
+//
+//  Created by Worker PC on 2/8/15.
+//  Copyright (c) 2015 Garvin Casimir. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class Cocoa_Bluetooth_RFCOMM_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
